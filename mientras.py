57afd6d4fb0b1bf47b7ab58bc050f1a23b6fe55e
julianarios03 <julianarios2611@gmail.com>
2023-03-04 13:18:05 -0500

    NT4

---
+++ mientras.py
@@ -0,0 +1,24 @@
+
+'''controladora=100
+
+print("Que se dice")
+print("*********************")
+print("1.Agregar sabor")
+print("2.Mostrar sabor")
+print("3.Cambiar sabor")
+print("0.Salir")
+
+
+while (controladora!=0):
+    helado=None
+    controladora=int(input("Digite un numero"))
+    if controladora==1:
+        helado=input("cual es el sabor: ")
+    elif controladora==2:
+        print(f'es abor es{helado}')
+    elif controladora==3:
+        helado=input("cual es el sabor: ")
+    elif controladora==0:
+        print("vuelva pronto")
+    else:
+        print("Opcion invalida")'''
